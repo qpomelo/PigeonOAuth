b6bc466eaa3b69b17bad9c96016d51bbf4dc21ac
QPomelo <bakaborange@gmail.com>
2020-03-10 16:24:10 +0800

    Initial Commit

---
+++ PigeonOAuth/PigeonOAuth.h
@@ -0,0 +1,19 @@
+//
+//  PigeonOAuth.h
+//  PigeonOAuth
+//
+//  Created by Pomelo on 10/3/2020.
+//  Copyright © 2020 QPomelo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PigeonOAuth.
+FOUNDATION_EXPORT double PigeonOAuthVersionNumber;
+
+//! Project version string for PigeonOAuth.
+FOUNDATION_EXPORT const unsigned char PigeonOAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PigeonOAuth/PublicHeader.h>
+
+
